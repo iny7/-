4a347b29ef59de57ee035f18afab64c0a5cb2e8e
iny7 <466924784@qq.com>
2015-12-30 19:59:07 +0800

    把背景架了起来
    （一两个月以前看到这个特效，觉得好玩看了一下，现在决定做前端啦，于是准备认真实现一遍练练手！）
    利用一些定位的知识做了一下背景

---
+++ 七夕/js/script.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+	var width = document.documentElement.offsetWidth;
+	var height = document.documentElement.offsetHeight;
+	console.log(width+'+'+height);
+	// alert(document.body.height)
+	console.log(document.body.style.)
+	var container1 = document.getElementById('container1');
+	// container1.style.width = 
+}
